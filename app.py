a90e35cf47fcb4bd50e4a511bcacaa04b0922357
ako-yuta <b1020125@fun.ac.jp>
2021-03-07 20:20:34 +0900

    [add]signup用のhtmlを作成

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = 'dIfk2EV4Ds7odVWK'
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup_form.html')
 
 
 if __name__ == '__main__':
